0a64bfa2d5bd708844191f30e71b2d65e85ce331
Michael Herman <hermanmu@gmail.com>
2015-07-02 13:22:42 -0600

    Site updated at 2015-07-02 19:22:42 UTC

--- javascripts/generate-toc.js
+++ javascripts/generate-toc.js
@@ -1,10 +1,10 @@
 function generateTOC(insertBefore, heading) {
-  var container = $("<div id='tocBlock' class='hidden-xs hidden-sm'></div>");
-  var div = $("<ul id='toc'></ul>");
+  var container = $("<div id='tocBlock'></div>");
+  var div = $("<ol id='toc'></ol>");
   var content = $(insertBefore).first();
 
   if (heading !== undefined && heading !== null) {
-    container.append('<h4 class="tocHeading">' + heading + '</h4>');
+    container.append('<h2 class="tocHeading">' + heading + '</h2>');
   }
 
   div.tableOfContents("#content",{startLevel: 2, depth: 1});
